8bba4e59175f36f9d5f741c8bddac8f29c723bc4
alejgarciarodriguez <alejgarciarodriguez@gmail.com>
2017-01-23 11:48:25 +0100

    añadido hammer.js

--- ts/gestures.ts
+++ ts/gestures.ts
@@ -10,11 +10,21 @@ const app = {
        const botonOscuro = document.querySelector("#oscuro");
        botonClaro.addEventListener("click",app.ponloClaro,false);
        botonOscuro.addEventListener("click",app.ponloOscuro,false);
+    },
+    iniciaHammer: () => {
+        const zona_gestos = document.getElementById("zona_gestos");
+        const hammer = new Hammer(zona_gestos);
+
+        const enable = { enable : true }
+        hammer.get("pinch").set( enable );
+        hammer.get("rotate").set( enable );
+
+        hammer.on("tap doubletap pan swipe press pinch rotate",
+        (ev) => document.querySelector("#info").innerHTML= ev.type+"!");
     }
 }
 
 app.inicio();
-
 
 if('addEventListener' in document){
     document.addEventListener('DOMContentLoaded',() => {
