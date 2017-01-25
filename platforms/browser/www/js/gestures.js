74b3ffdf0af3674f4ef6349affccab9423dff351
Alejgarcia <alejgarciarodriguez@gmail.com>
2017-01-25 12:09:34 +0100

    Css tañ añadido

--- platforms/browser/www/js/gestures.js
+++ platforms/browser/www/js/gestures.js
@@ -13,14 +13,13 @@ var app={
     var botonClaro = document.querySelector('#claro');
     var botonOscuro = document.querySelector('#oscuro');
     
-    botonClaro.addEventListener('click',this.ponloClaro,false);
+    botonClaro.addEventListener('click',app.ponloClaro,false);
     botonOscuro.addEventListener('click',app.ponloOscuro,false);
   },
 
   iniciaHammer: function() {
     var zona = document.getElementById('zona-gestos');
     var hammertime = new Hammer(zona);
-    
     hammertime.get('pinch').set({ enable: true });
     hammertime.get('rotate').set({ enable: true });
 
@@ -32,6 +31,10 @@ var app={
       zona.className='doubletap';
     });
 
+    hammertime.on('tap', function(ev){
+      zona.className='tap';
+    });
+
     hammertime.on('press', function(ev) {
       zona.className='press';
     });
@@ -39,17 +42,18 @@ var app={
     hammertime.on('swipe', function(ev) {
       var clase=undefined;
       direccion=ev.direction;
-      
+
       if (direccion==4) clase='swipe-derecha';
       if (direccion==2) clase='swipe-izquierda';
       
       zona.className=clase;
     });
 
-
     hammertime.on('rotate', function(ev) {
       var umbral=25;
-      if (ev.distance > umbral) zona.className='rotate';
+      if (ev.distance > umbral){
+        zona.className='rotate';
+      }
     });
   },
 
